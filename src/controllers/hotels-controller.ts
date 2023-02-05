89c4a0ca6af24af58ac99a086c06928fd06c65de
Nicolas Araújo <nicolasaraujo@gmail.com>
2023-02-05 15:55:39 -0300

    feat: validações necessarias ao pegar a lista de hoteis

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -1,11 +1,26 @@
 import hotelsService from '@/services/hotels-service';
 import { AuthenticatedRequest } from '@/middlewares';
 import { Response } from 'express';
+import httpStatus from 'http-status';
 
 export async function getHotels(req: AuthenticatedRequest, res: Response) {
-  const hotels = await hotelsService.fetchHotels();
+  const userId: number = req.userId;
 
-  res.status(200).send(hotels);
+  try {
+    await hotelsService.checkIfEnrollmentAndTicketAreValid(userId);
+
+    const hotels = await hotelsService.fetchHotels();
+
+    res.status(200).send(hotels);
+  } catch (err) {
+    if (err.name === 'NotFoundError') {
+      res.status(httpStatus.NOT_FOUND).send(err.message);
+    }
+
+    if (err.name === 'paymentRequired') {
+      res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+    }
+  }
 }
 
 export async function getRoomsByHotel() {}
--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -1,11 +1,27 @@
 import hotelsRepository from '@/repositories/hotels-repository';
+import enrollmentRepository from '@/repositories/enrollment-repository';
+import ticketRepository from '@/repositories/ticket-repository';
+import { notFoundError } from '@/errors';
+
+async function checkIfEnrollmentAndTicketAreValid(userId: number) {
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  if (!enrollment) throw notFoundError();
+
+  const ticket = await ticketRepository.findTicketByEnrollmentId(enrollment.id);
+  if (!ticket) throw notFoundError();
+
+  if (ticket.status !== 'PAID') throw { name: 'paymentRequired' };
+  if (!ticket.TicketType.includesHotel) throw { name: 'paymentRequired' };
+}
 
 async function fetchHotels() {
   const hotels = await hotelsRepository.fetchHotels();
 
+  if (hotels.length === 0) throw notFoundError();
+
   return hotels;
 }
 
-const hotelsService = {fetchHotels};
+const hotelsService = { fetchHotels, checkIfEnrollmentAndTicketAreValid };
 
 export default hotelsService;
